83f28401a151363cc4b684879ff3c71cf641f77f
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-03 16:52:48 +0100

    feat:struttura del esercizio

---
+++ Palidroma/js/script.js
@@ -0,0 +1,25 @@
+// Esercizio 1 //
+
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma 
+// (NON usare split, reverse e join)
+
+
+// 1. Chiedamo al utente di inserire una parola, usiamo prompt
+// 2. Creiamo la funzione per verificare si la parola è palidroma
+// parametro: parola che l'utente inserisce
+// return: stringa, La parola è palindroma o La parola non è palindroma
+// Dobbiamo ciclare il ciclo FOR per confrontare la parola con la sua versione 
+// al contrario 
+
+const parolaUtente = prompt("Ciao, inserisce una parola");
+
+function isPalidroma(parola) {
+    let parolaInversa = "";
+
+    for (let i = parola.length - 1; i >= 0; i--) {
+        let parolaInversa = parolaInversa + parola[i];
+        console.log (i);
+    }
+} 
